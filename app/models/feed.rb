54079469475fd2e205ff7b6f2ffefc9419a14ba2
Jordan Bracco <jordan@bracco.name>
2009-03-08 16:10:06 +0100

    Add feed_url and url in Feed model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,8 @@ class Feed
   
   property :id, Serial
   property :name, String, :unique => true, :nullable => :false
+  property :feed_url, String, :unique => true, :nullable => :false, :length => 255
+  property :url, String, :unique => true, :nullable => :false, :length => 255
 
   def self.find_by_name(name)
     all(:name => name, :order => [:id.desc])
