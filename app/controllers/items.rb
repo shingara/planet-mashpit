a02e4fad3cf0cb04f487a06dbbe7ac8b3053987c
Jordan Bracco <jordan@bracco.name>
2009-03-10 00:40:24 +0100

    Style like Helvetireader

--- app/controllers/items.rb
+++ app/controllers/items.rb
@@ -1,10 +1,10 @@
 class Items < Application
 
   cache :index
-
+  
   def index
     @current_page = params[:page] ? params[:page].to_i : 1
-    @page_count, @items = Item.paginated :page => @current_page, :per_page => 2, :order => [:created_at.desc]
+    @page_count, @items = Item.paginated :page => @current_page, :per_page => 20, :order => [:created_at.desc]
     display @items
   end
   
